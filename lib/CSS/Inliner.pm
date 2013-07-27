aed283c0b8391d43b01c0a41b26d2f7d47fb79c0
Kevin H. Kamel <kamelkev@underprint.com>
2013-07-26 20:28:29 -0400

    MM-8488
    Update module description and copyright with proper year. This is a maintained module, so keeping these up to date
    is something we need to spend more time on...
    
    Additionally strip out "post_fetch_filter". This was an undocumented feature that was used internally by the module
    sponsor. It is no longer used there and may now be removed.

--- lib/CSS/Inliner.pm
+++ lib/CSS/Inliner.pm
@@ -41,13 +41,13 @@ print $inliner->inlinify();
 
 Library for converting CSS style blocks into inline styles in an HTML
 document.  Specifically this is intended for the ease of generating
-HTML emails.  This is useful as even in 2009 Gmail and Hotmail don't
+HTML emails.  This is useful as even in 2013 Gmail and Hotmail don't
 support top level <style> declarations.
 
 =cut
 
 BEGIN {
-  my $members = ['stylesheet','css','html','html_tree','query','strip_attrs','leave_style','warns_as_errors','content_warnings', 'post_fetch_filter'];
+  my $members = ['stylesheet','css','html','html_tree','query','strip_attrs','leave_style','warns_as_errors','content_warnings'];
 
   #generate all the getter/setter we need
   foreach my $member (@{$members}) {
@@ -82,8 +82,6 @@ B<strip_attrs> (optional). Remove all "id" and "class" attributes during inlinin
 
 B<leave_style> (optional). Leave style/link tags alone within <head> during inlining
 
-B<post_fetch_filter> (optional). Execute a coderef filter on fetched content. Useful for protecting mailmerge tags while fetching. You are responsible for restoring any tags in your own code
-
 =back
 
 =cut
@@ -103,7 +101,6 @@ sub new {
     strip_attrs => (defined($$params{strip_attrs}) && $$params{strip_attrs}) ? 1 : 0,
     leave_style => (defined($$params{leave_style}) && $$params{leave_style}) ? 1 : 0,
     warns_as_errors => (defined($$params{warns_as_errors}) && $$params{warns_as_errors}) ? 1 : 0,
-    post_fetch_filter => (defined($$params{post_fetch_filter}) && ref($$params{post_fetch_filter}) eq 'CODE') ? $$params{post_fetch_filter} : undef
   };
 
   bless $self, $class;
@@ -515,10 +512,6 @@ sub _fetch_html {
 
   my ($content,$baseref) = $self->_fetch_url({ url => $$params{url} });
 
-  if (my $post_fetch_filter = $self->_post_fetch_filter()) {
-    $content = &$post_fetch_filter({ html => $content });
-  }
-
   # Build the HTML tree
   my $doc = HTML::TreeBuilder->new();
   $doc->parse($content);
@@ -787,7 +780,7 @@ Michael Peters <C<wonko@cpan.org>>
 
 =head1 LICENSE
 
-This module is Copyright 2011 Khera Communications, Inc.  It is
+This module is Copyright 2013 Khera Communications, Inc.  It is
 licensed under the same terms as Perl itself.
 
 =cut
